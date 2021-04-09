25632e640b4bbafb6a70bf3ed6ca318be1c78355
Aman Hirpara <ahirpara2000@gmail.com>
2021-04-09 18:05:53 +0000

    Added login button and linked it to google's oauth api

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <login />
+      <Login />
     </div>
   );
 }
--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,37 @@
 import React from 'react';
 
-export function Login() 
-{
-    return <div></div>;
-}
+import { GoogleLogin } from 'react-google-login';
 
+const clientId = '1011493052864-t6p5q2p2mcfaif046nhcaa7qf8ap2hab.apps.googleusercontent.com';
+
+export function Login() {
+  const onSuccess = (res) => {
+    console.log('Login Success: currentUser:', res.profileObj);
+    alert(
+      `Logged in successfully welcome ${res.profileObj.name} 😍. \n See console for full profile object.`,
+    );
+  };
+
+  const onFailure = (res) => {
+    console.log('Login failed: res:', res);
+    alert(
+      'Failed to login. 😢 Please ping this to repo owner twitter.com/sivanesh_fiz',
+    );
+  };
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy="single_host_origin"
+        style={{ marginTop: '100px' }}
+        isSignedIn
+      />
+    </div>
+  );
+}
 
 export default Login;
