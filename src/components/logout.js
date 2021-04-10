fdead3377f3efef6f20466c90603173c28947f0b
Aman Hirpara <ahirpara2000@gmail.com>
2021-04-10 15:40:56 +0000

    Added logout files

--- src/components/logout.js
+++ src/components/logout.js
@@ -2,12 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { GoogleLogout } from 'react-google-login';
 
-const clientId = '707788443358-u05p46nssla3l8tmn58tpo9r5sommgks.apps.googleusercontent.com';
+const clientId = '1011493052864-t6p5q2p2mcfaif046nhcaa7qf8ap2hab.apps.googleusercontent.com';
 
 export function GLogout({ setLoginStatus }) {
   const onSuccess = () => {
     console.log('Logout made successfully');
-    alert('Logout made successfully ✌');
     setLoginStatus(false);
   };
 
