9235a255a6d7c33b5bc61af401ab8eaf21a4391f
Aman Hirpara <ahirpara2000@gmail.com>
2021-04-21 01:40:44 +0000

    Replaced dummy text

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -19,14 +19,16 @@ export function LandingPage({ setLoginStatus })
                 <img src={ ClipBoard } />
             </div>
             <div className="textContent">
-                <h2>Have a visual understanding of your expenses</h2>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <h2>Manage your expenses in one place</h2>
+                <p>Easily add, remove or update a log your income or expense. Access all your added incomes and expenses with details, 
+                   including the amount, date, where you spent or got the money from, and a description.</p>
             </div>
           </section>
           <section className="feature2">
             <div className="textContent">
-                <h2>Keep track of all your expenses</h2>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <h2>Keep track of all your spendings</h2>
+                <p>Our application does all the calculations for you. You can access your total balance, income, and expenses. 
+                   You can also set a budget for a week, month, or year, and the app will let you know if you go over budget.</p>
             </div>
             <div>
                 <img src={icon} />
@@ -38,7 +40,8 @@ export function LandingPage({ setLoginStatus })
             </div>
             <div className="textContent">
                 <h2>Have a visual understanding of your expenses</h2>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <p>To get a quick overlay of your expense, look at the provided charts which shows you your weekly, monthly, 
+                   or annual incomes and expenses side-by-side. Or take a look at the pie chart which displays all your spendings based on categories.</p>
             </div>
           </section>
         </div>
