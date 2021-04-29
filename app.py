49118035a836913949b8fb780771b52e1413ad1f
Mihir240 <mihirr240@gmail.com>
2021-04-29 02:18:39 +0000

    Added year and month filtering for income and expense

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask import request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv, find_dotenv
 from db_api import *
+from datetime import datetime, timedelta
 
 load_dotenv(find_dotenv())  # This is to load your env variables from .env
 APP = Flask(__name__, static_folder="./build/static")
@@ -113,39 +114,100 @@ def get_chart_info():
     # print(sums)
     
     sums = []
+    sum_income = []
+    sum_expense_month = []
+    sum_income_month = []
     expense_cat = []  
+    income_cat = []
+    expense_cat_month = []
+    income_cat_month = []
     
     print(categories)
     print(transactions)
     
+    #removing duplicate categories
     new_categories = []
     for item in categories:
         if item not in new_categories:
             new_categories.append(item)
     
     print(new_categories)
-    flag_expense = False
+    flag_expense_year = False
+    flag_income_year = False
+    flag_expense_month = False
+    flag_income_month = False
+    
+    restrict_today = datetime.today().strftime("%Y-%m-%d")
+    restrict_today = datetime.date(datetime.strptime(restrict_today,'%Y-%m-%d'))
+    past_month = (datetime.today() - timedelta(30)).strftime("%Y-%m-%d")
+    past_year = (datetime.today() - timedelta(365)).strftime("%Y-%m-%d")
+    past_month = datetime.date(datetime.strptime(past_month, '%Y-%m-%d'))
+    past_year = datetime.date(datetime.strptime(past_year, '%Y-%m-%d'))
+    
+    
+    # print(type(datetime_object))
+    # print(past_year)
+    
     for category in new_categories: #go thru all of the categories
-        li = []
+        li_expense_year = []
+        li_income_year = []
+        li_expense_month = []
+        li_income_month = []
+        
         for transaction in transactions:
-            if transaction["category"] == category and transaction["type"] == "Expense":
-                 flag_expense = True
+            
+            if transaction["category"] == category and transaction["type"] == "Expense" and transaction['date'] >= past_year and transaction['date'] <= restrict_today:
+                 print(transaction["date"])
+                 flag_expense_year = True
                  print(transaction["type"])
-                 li.append(transaction["amount"])
-                #  sums.append(sum(li))
-                 
+                 li_expense_year.append(transaction["amount"])
                  if category not in expense_cat:
                     expense_cat.append(category)
-         
-        if flag_expense:    
-            sums.append(sum(li))
-            flag_expense = False
+            
+            if transaction["category"] == category and transaction["type"] == "Income" and transaction['date'] >= past_year and transaction['date'] <= restrict_today:
+                flag_income_year = True
+                li_income_year.append(transaction["amount"])
+                if category not in income_cat:
+                    income_cat.append(category)
+            
+            if transaction["category"] == category and transaction["type"] == "Expense" and transaction['date'] >= past_month and transaction['date'] <= restrict_today:
+                flag_expense_month = True
+                li_expense_month.append(transaction["amount"])
+                if category not in expense_cat_month:
+                    expense_cat_month.append(category)
+                    
+            if transaction["category"] == category and transaction["type"] == "Income" and transaction['date'] >= past_month and transaction['date'] <= restrict_today:
+                flag_income_month = True
+                li_income_month.append(transaction["amount"])
+                if category not in income_cat_month:
+                    income_cat_month.append(category)
+                    
+                    
+        if flag_expense_year:    
+            sums.append(sum(li_expense_year))
+            flag_expense_year = False
+            
+        if flag_income_year:
+            sum_income.append(sum(li_income_year))
+            flag_income_year = False
+            
+        if flag_expense_month:
+            sum_expense_month.append(sum(li_expense_month))
+            flag_expense_month = False
+            
+        if flag_income_month:
+            sum_income_month.append(sum(li_income_month))
+            flag_income_month = False
+        
         
     print(sums)
     print(expense_cat)
+    print(sum_income)
+    print(income_cat)
     
-        
-    return jsonify([expense_cat,sums])
+                    #expense year , value| income year, value | expense month , values | income month, values
+    return jsonify([expense_cat,sums, income_cat, sum_income,  expense_cat_month, sum_expense_month, income_cat_month, sum_income_month])
+    # return jsonify({transactions,categories)
     
 
 
--- src/components/charts.js
+++ src/components/charts.js
@@ -1,10 +1,50 @@
+/* eslint-disable */
 import React, { useState, useEffect } from "react";
-import { Doughnut } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 
 export function Charts()
 {   
-    const [chartData, setChartData] = useState([[], []]);
+    const [chartData, setChartData] = useState([[], [], [], [], [], [], [], []]);
+    const [filterOptions, setFilterOptions] = useState("Year");
+    const [labelIncome, setIncome] = useState([[],[]]);
+    const [labeExpense, setExpense] = useState([[],[]]);
+    /*
+        Expense Year
+      0: labels
+      1: values
+      
+        Income Year
+      2: labels
+      3: values
+      
+        Expense Month
+      4: labels
+      5: values
+      
+        Income Month
+      6: labels
+      7: values
+     */
     
+      function handleFilter(e) {
+         if (e.target.value === "Month") {
+          /*indices 4-7*/
+          setExpense([chartData[4],chartData[5]]);
+          setIncome([chartData[6],chartData[7]]);
+          
+        } else if (e.target.value === "Year") {
+          /*indice 0-3*/
+          setExpense([chartData[0],chartData[1]]);
+          setIncome([chartData[2],chartData[3]]);
+          
+        } else{
+            setExpense([chartData[0],chartData[1]]);
+            setIncome([chartData[2],chartData[3]]);
+        }
+      }
+    
+    
+  
      function getData() {
         fetch("/chartInfo")
           .then((res) => res.json())
@@ -13,6 +53,9 @@ export function Charts()
             console.log(val[0]);
             console.log(val[1]);
             setChartData(val);
+            setExpense([val[0],val[1]]);
+            setIncome([val[2],val[3]]);
+
           });
       }
       
@@ -22,10 +65,10 @@ export function Charts()
     },[]);
     
     const state = {
-      labels: chartData[0],
+      labels: labeExpense[0],
       datasets: [
         {
-          label: 'Rainfall',
+          label: 'Expense',
           backgroundColor: [
             '#98ddca',
             '#d5ecc2',
@@ -49,19 +92,66 @@ export function Charts()
 
           ],
           hoverOffset: 5,
-          data: chartData[1],
+          data: labeExpense[1],
+        }
+      ]
+    }
+    const stateIncome = {
+      labels: labelIncome[0],
+      datasets: [
+        {
+          label: 'Income',
+          backgroundColor: [
+            '#98ddca',
+            '#d5ecc2',
+            '#ffd3b4',
+            '#ffaaa7',
+            '#eca3f5', 
+            '#fdbaf8', 
+            '#b0efeb', 
+            '#edffa9'
+
+          ],
+          hoverBackgroundColor: [
+          '#98ddca',
+          '#d5ecc2',
+          '#ffd3b4',
+          '#ffaaa7',
+          '#eca3f5', 
+          '#fdbaf8', 
+          '#b0efeb', 
+          '#edffa9'
+
+          ],
+          hoverOffset: 5,
+          data: labelIncome[1],
         }
       ]
     }
     
     return (
         <div>
+          <select
+              value={filterOptions}
+              onChange={(e) => {
+                handleFilter(e);
+                setFilterOptions(e.target.value);
+              }}>
+              <option value="Year"> Show Year</option>
+              <option value="Month"> Show Month</option>
+          </select>
             <h1>Charts</h1>
-            <Doughnut
+            <Bar
                 data={state}
                 height={400}
                 width={600}
             />
+            <Bar
+                data={stateIncome}
+                height={400}
+                width={600}
+            />
+            
         </div>
     );
 }
