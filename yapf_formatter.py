5120f8916a30a84a01ea33f804332d74b3e3a085
Abishek Vasudevan <harishabishek7@gmail.com>
2021-05-01 04:10:37 +0000

    Formatting

--- yapf_formatter.py
+++ yapf_formatter.py
@@ -4,14 +4,19 @@ Formats all .py files from file's directory recursively.
 # pylint: skip-file
 import os
 import re
+import sys
 from yapf.yapflib.yapf_api import FormatFile
 
-PATH = '.'
+PATHS = '.' if len(sys.argv) == 0 else sys.argv[1:]
 FILES = []
-for r, d, f in os.walk(PATH):
-    for file in f:
-        if re.match(r".*.py$", file):
-            FILES.append(os.path.join(r, file))
-for filename in FILES:
-    print(f"Formatting: {filename}")
-    FormatFile(filename, in_place=True)
+for PATH in PATHS:
+    for r, d, f in os.walk(PATH):
+        for file in f:
+            if re.match(r".*.py$", file):
+                FILES.append(os.path.join(r, file))
+    for filename in FILES:
+        print(f"Formatting: {filename}")
+        try:
+            FormatFile(filename, in_place=True)
+        except:
+            print("Error! Continuing with other files!")
